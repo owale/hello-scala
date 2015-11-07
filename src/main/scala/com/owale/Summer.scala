cd7b2b45b64fe289d2347336748940f535f3b309
waleedsamy <waleed_samy@ymail.com>
2015-11-08 00:10:06 +0200

    Writing new control structures

--- src/main/scala/com/owale/Summer.scala
+++ src/main/scala/com/owale/Summer.scala
@@ -1,7 +1,14 @@
 package com.owale
 
-import utils.grep
+import java.io.File
+
+import com.owale.utils._
 
 object Summer extends App{
   grep(".*gcd*.")
+
+  val file = new File("date.txt")
+  withPrintWriter(file){
+    writer => writer.println(new java.util.Date)
+  }
 }
--- src/main/scala/com/owale/utils.scala
+++ src/main/scala/com/owale/utils.scala
@@ -1,20 +1,29 @@
 package com.owale
 
-import java.io.File;
+import java.io.{PrintWriter, File}
 
-object utils{
+object utils {
+
+  def grep(pattern: String, where: String = ".") = {
+    val filesHere = (new File(where)).listFiles
+    for (file <- filesHere
+         if file.isFile
+         if file.getName.endsWith(".scala");
+         line <- getLines(file);
+         trimmed = line.trim
+         if trimmed.matches(pattern)
+    ) println(file + " @ " + trimmed)
+  }
 
   def getLines(file: File) = scala.io.Source.fromFile(file).getLines().toList
 
-  def grep(pattern: String , where:String = ".") = {
-    val filesHere = (new File(where)).listFiles
-    for(file<- filesHere
-      if file.isFile
-      if file.getName.endsWith(".scala");
-      line<- getLines(file);
-      trimmed = line.trim
-      if trimmed.matches(pattern)
-    ) println(file +" @ "+ trimmed)
+  def withPrintWriter(file: File)(op: PrintWriter => Unit) {
+    val writer = new PrintWriter(file)
+    try {
+      op(writer)
+    } finally {
+      writer.close()
+    }
   }
 
 }
